5f9c24e8980d3ca63488aa569f1536f60633de3d
Ritesh Gupta <152794543+RiteshGupta19@users.noreply.github.com>
2025-04-16 21:37:08 +0530

    Update App.jsx

--- todo-docker-jenkins/frontend/src/App.jsx
+++ todo-docker-jenkins/frontend/src/App.jsx
@@ -68,7 +68,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Todo List</h1>
+      <h1>Todo Listt</h1>
       <div className="input-container">
         <input
           type="text"
