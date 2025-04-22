294fdc6cb92941cd2bb1d4f0728270deaba82ddc
Ritesh Gupta <152794543+RiteshGupta19@users.noreply.github.com>
2025-04-22 23:27:38 +0530

    Update vite.config.js

--- todo-docker-jenkins/frontend/vite.config.js
+++ todo-docker-jenkins/frontend/vite.config.js
@@ -21,18 +21,17 @@
 // })
 
 
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // This makes your app accessible externally
     allowedHosts: ['43.204.131.219', 'localhost', 'frontend_cluster'], // Add the IP and hostname you want to allow
     hmr: {
-      host: '43.204.131.219', // Set the WebSocket host for HMR
-      port: 5173, // Use the correct WebSocket port
+      host: '43.204.131.219', // This ensures WebSocket connects to public IP
+      port: 5173, // WebSocket port
     },
     proxy: {
       '/api': 'http://43.204.131.219:5000', // Proxy API requests to backend
