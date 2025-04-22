e65268ad7fb25ae4b9cc4084024beffcef3aca70
Ritesh Gupta <152794543+RiteshGupta19@users.noreply.github.com>
2025-04-22 23:03:43 +0530

    Update vite.config.js

--- todo-docker-jenkins/frontend/vite.config.js
+++ todo-docker-jenkins/frontend/vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This makes your app accessible externally
+    allowedHosts: ['43.204.131.219', 'localhost', 'frontend_cluster'], // Add the IP and hostname you want to allow
+  },
 })
